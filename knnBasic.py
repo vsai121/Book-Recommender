1795cfad78b5a6cbb933aca3f346ae1398730f1e
Saicharan <vsaicharan1998@gmail.com>
2018-04-02 15:44:41 +0530

    Computed similarity matrix

--- knnBasic.py
+++ knnBasic.py
@@ -8,49 +8,64 @@ from surprise.model_selection import cross_validate
 from surprise import KNNBasic , KNNWithMeans , KNNWithZScore , KNNBaseline
 from surprise import accuracy
 from surprise.model_selection import train_test_split
-
+from six import iteritems
 
 from collections import defaultdict
+from similarity import cosine
 
 print("changed")
 shape =  (12001 , 10001)
 
 br_cols = ['book_id' , 'user_id' , 'rating']
 bookRatings = pd.read_csv('Data/ratings.csv' , sep=',' , names = br_cols , encoding='latin-1' , low_memory=False , skiprows=[0])
-bookRatings = bookRatings[['user_id' , 'book_id' , 'rating']]
+bookRatings = bookRatings[['user_id' ,  'book_id' , 'rating']]
 
-print(bookRatings.head())
 
 #print(ratings.head())
 
 b_cols = ['book_id' , 'books_count' , 'isbn' , 'isbn13' , 'authors' , 'original_publication_year' , 'original_title' , 'average_rating' , 'ratings_count' , 'image_url']
 books = pd.read_csv('Data/books.csv' , sep=',' , usecols=b_cols , encoding='latin-1' , low_memory = False)
 
-print(books.head())
+#print(books.head())
 
 tr_cols = ['user_id' , 'book_id']
 to_read = pd.read_csv('Data/to_read.csv' , sep=',' , usecols=tr_cols , encoding='latin-1' , low_memory = False)
 #print(books.head())
-
+"""
 print(bookRatings.shape)
 print(books.shape)
 print(to_read.shape)
-
+"""
 
 bookRatings = bookRatings.drop_duplicates(['user_id' , 'book_id'] , 'first')
 bookRatings.groupby('user_id').filter(lambda x: len(x) >= 4)
+bookRatings = bookRatings[bookRatings['user_id']<=15000]
+
+itemRatings = bookRatings[['book_id' , 'rating']]
+itemRatings.set_index('book_id' , inplace=True)
+
+print("Item ratings")
+print(itemRatings.head())
 
 row_list=[]
 
-for index,row in to_read.iterrows():
+print(bookRatings.shape)
+print("Entered similarity")
+sim = cosine(10000 , itemRatings , 1)
+print(sim)
+
+"""
+for ratings in iteritems(bookRatings):
 
     #print(row['user_id'])
     #print(row['book_id'])
     dict1={}
 
+    print((ratings))
 
-    uid = int(row['user_id'])
-    bid = int(row['book_id'])
+
+    #uid = int(row['user_id'])
+    #bid = int(row['book_id'])
 
     #print('User id' + str(uid))
     #print('Book Id' + str(bid))
@@ -59,16 +74,18 @@ for index,row in to_read.iterrows():
         avg_rating = books.loc[bid]['average_rating']
         dict.update({'user_id':uid , 'book_id':bid , 'rating':avg_rating})
 
-        if uid <=50000:
+        if uid <=1000:
             row_list.append(dict1)
 
         else:
             break;
+    """
 
+"""
 df = pd.DataFrame(row_list)
 bookRatings.append(df , ignore_index = True)
 
-bookRatings = bookRatings[bookRatings['user_id']<=50000]
+bookRatings = bookRatings[bookRatings['user_id']<=1000]
 print(bookRatings.shape)
 
 
@@ -86,6 +103,7 @@ sim_options = {
 }
 
 knn = KNNWithZScore(k = 100 , min_k = 1 ,sim_options=sim_options)
+
 knn.fit(trainingSet)
 
 predictions = knn.test(testSet)
@@ -140,6 +158,7 @@ for uid, user_ratings in top3_recommendations.items():
 
 print(accuracy.rmse(predictions))
 
+"""
 """
 Basic
 
